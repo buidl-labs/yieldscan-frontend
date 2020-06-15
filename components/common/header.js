02d4e75cd0972afefc3d22eecb40590130cf4787
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-15 06:18:19 +0530

    feat: 🎸 header style updated: replaced shadow with border

--- components/common/header.js
+++ components/common/header.js
@@ -1,6 +1,6 @@
 const Header = () => {
 	return (
-		<div className="flex items-center justify-between shadow bg-white p-8 h-12">
+		<div className="flex items-center justify-between border border-bottom border-gray-200 bg-white p-8 h-12">
 			<div>
 				<span className="text-lg text-black">YieldScan</span>
 			</div>
