1e38dad5b55736512b283325f222159e20dcfffe
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-15 09:18:54 +0530

    feat: 🎸 developed expected-returns-card for calculator page

--- components/reward-calculator/index.js
+++ components/reward-calculator/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "@lib/axios";
-import { ChevronDown } from "react-feather";
+import { ChevronDown, HelpCircle } from "react-feather";
 
 const AmountInput = () => {
 	return (
@@ -71,6 +71,42 @@ const TimePeriodInput = () => {
 	);
 };
 
+const ResultCardInsight = ({ label, value, supportValue }) => (
+	<div className="mt-2 mr-10">
+		<div className="flex-center">
+			<span className="opacity-75 mr-1">{label}</span>
+			<HelpCircle size="1rem" cursor="pointer" strokeOpacity="0.75" />
+		</div>
+		<h3 className="text-2xl">{value}</h3>
+		<span className="text-sm">{supportValue}</span>
+	</div>
+);
+
+const ExpectedReturnsCard = () => (
+	<div className="rounded-xl bg-teal-500 text-white px-10 py-6">
+		<h1 className="font-semibold text-2xl">Expected Returns</h1>
+		<div className="flex flex-wrap mt-2">
+			<ResultCardInsight
+				label="Estimated Returns"
+				value="100 KSM"
+				supportValue="$50"
+			/>
+			<ResultCardInsight
+				label="Estimated Portfolio Value"
+				value="1000 KSM"
+				supportValue="$500"
+			/>
+			<ResultCardInsight
+				label="Estimated Yield"
+				value="5.46%"
+			/>
+		</div>
+		<button className="rounded-full font-semibold bg-white text-teal-500 text-lg mt-5 px-8 py-3">
+			Connect Wallet to Stake
+		</button>
+	</div>
+);
+
 const RewardCalculatorPage = () => {
 	const [validatorMap, setValidatorMap] = useState({}); // map with low/med/high risk sets
 	const [estimatedReward, setEstimatedReward] = useState('');
@@ -123,7 +159,7 @@ const RewardCalculatorPage = () => {
 				</div>
 			</div>
 			<div className="w-1/2">
-				
+				<ExpectedReturnsCard />
 			</div>
 		</div>
 	);
