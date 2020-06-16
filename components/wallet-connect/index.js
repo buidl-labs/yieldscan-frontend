b669644fae111d753b437e40d51c279f685235f8
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-16 22:26:55 +0530

    feat: 🎸 implemented design: intro-modal for wallet-connect

--- components/wallet-connect/index.js
+++ components/wallet-connect/index.js
@@ -1,23 +1,33 @@
-import { Modal, ModalBody, ModalOverlay, ModalContent } from '@chakra-ui/core';
-import { createContext } from 'react';
+import { Modal, ModalBody, ModalOverlay, ModalContent, ModalCloseButton } from '@chakra-ui/core';
 import { create } from 'zustand';
 
 const [useWalletConnect] = create(set => ({
-	isOpen: false,
+	isOpen: true,
 	toggle: () => set(state => ({ isOpen: !state.isOpen })),
 	close: () => set(state => ({ isOpen: false })),
 	open: () => set(state => ({ isOpen: true })),
 }));
 
+const IntroPage = () => (
+	<div className="mx-10 my-10 flex flex-col text-center items-center">
+		<img src="images/polkadot-wallet-connect.png" width="200px" />
+		<h3 className="mt-4 text-2xl">Connect to the PolkaJS Wallet to Stake your KSM Tokens</h3>
+		<span className="mt-1 px-4 text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</span>
+		<button className="mt-8 px-24 py-4 bg-teal-500 text-white rounded-lg">Connect my wallet</button>
+		<button className="mt-2 px-12 py-4 bg-white text-teal-500 rounded-lg border border-teal-500">Create a wallet for my account</button>
+		<a href="#" className="mt-8 text-gray-500">How can I obtain KSM tokens?</a>
+	</div>
+);
+
 const WalletConnectPopover = () => {
-	const isOpen = useWalletConnect(state => state.isOpen);
-	console.log(isOpen);
+	const { isOpen, close } = useWalletConnect();
 	return (
-		<Modal isOpen={isOpen} onClose={}>
+		<Modal isOpen={isOpen} onClose={close} isCentered>
 			<ModalOverlay />
-			<ModalContent>
+			<ModalContent rounded="lg" maxWidth="33rem">
+				<ModalCloseButton />
 				<ModalBody>
-					<h1>Hello</h1>
+					<IntroPage />
 				</ModalBody>
 			</ModalContent>
 		</Modal>
