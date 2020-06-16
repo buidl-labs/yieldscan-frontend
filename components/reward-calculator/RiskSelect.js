3468e5c67b8a62b9e78e2d3605770e731fae2b62
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-16 05:52:01 +0530

    feat: 🎸 updated design of risk-select

--- components/reward-calculator/RiskSelect.js
+++ components/reward-calculator/RiskSelect.js
@@ -1,11 +1,12 @@
-const RiskSelect = ({ selected = 'Low' }) => {
+const RiskSelect = ({ selected = 'Medium' }) => {
 	const options = ['Low', 'Medium', 'High'];
 	return (
-		<div className="flex rounded-full border border-gray-300 py-2 px-2 w-2/3">
+		<div className="relative flex rounded-full border border-gray-300 py-2 px-2 w-2/3">
+			<div className="absolute left-0 h-100 mx-2 my-2 rounded-full w-1/3 bg-orange-500"></div>
 			{options.map(option => (
 				<span key={option} className={`
-					w-1/3 font-semibold py-4 flex-center rounded-full cursor-pointer
-					${selected === option ? 'bg-orange-500 text-white' : 'text-gray-600'}
+					w-1/3 font-semibold py-4 mx-1 flex-center rounded-full cursor-pointer z-50 transition duration-200
+					${selected === option ? 'bg-orange-500 text-white' : 'text-gray-600 hover:bg-orange-200'}
 				`}>
 					{option}
 				</span>
