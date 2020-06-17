2922bf3fe4b16d5e7deb6c6fff5ab63b9441534f
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-17 21:05:13 +0530

    feat: 🎸 risk-select component now works dynamically

--- components/reward-calculator/RiskSelect.js
+++ components/reward-calculator/RiskSelect.js
@@ -1,13 +1,17 @@
-const RiskSelect = ({ selected = 'Medium' }) => {
+const RiskSelect = ({ selected = 'Medium', setSelected }) => {
 	const options = ['Low', 'Medium', 'High'];
 	return (
 		<div className="relative flex rounded-full border border-gray-300 py-2 px-2 w-2/3">
 			<div className="absolute left-0 h-100 mx-2 my-2 rounded-full w-1/3 bg-orange-500"></div>
 			{options.map(option => (
-				<span key={option} className={`
-					w-1/3 font-semibold py-4 mx-1 flex-center rounded-full cursor-pointer z-50 transition duration-200
-					${selected === option ? 'bg-orange-500 text-white' : 'text-gray-600 hover:bg-orange-200'}
-				`}>
+				<span
+					key={option}
+					className={`
+						w-1/3 font-semibold py-4 mx-1 flex-center rounded-full cursor-pointer z-50 transition duration-200
+						${selected === option ? 'bg-orange-500 text-white' : 'text-gray-600 hover:bg-orange-200'}
+					`}
+					onClick={() => setSelected(option)}
+				>
 					{option}
 				</span>
 			))}
