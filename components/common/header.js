63232b66c6e9f143c4f8f042517ed59e840e1cd2
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-19 06:08:56 +0530

    feat: 🎸 header is now dynamic -> updates balance and account

--- components/common/header.js
+++ components/common/header.js
@@ -1,15 +1,29 @@
+import { useAccounts } from "@lib/store";
+import { get, isNil } from "lodash";
+import { ChevronRight } from "react-feather";
+
 const Header = () => {
+	const { stashAccount, freeAmount } = useAccounts();
+	console.log(stashAccount, freeAmount);
 	return (
 		<div className="flex items-center justify-between border border-bottom border-gray-200 bg-white p-8 h-12">
 			<div>
 				<span className="text-lg text-black">YieldScan</span>
 			</div>
-			<div>
-				<button className="rounded-full border border-gray-300 p-2 px-4 font-semibold text-gray-800 mr-2">
-					Connect Wallet
-				</button>
-				<button className="rounded-full border border-gray-300 p-2 px-4 font-semibold text-gray-800">
-					>
+			<div className="flex items-center mr-16">
+				{isNil(stashAccount) ? (
+					<button className="rounded-full border border-gray-300 p-2 px-4 font-semibold text-gray-800 mr-2">
+						Connect Wallet
+					</button>
+				) : (
+					<div>
+						<h3 className="text-gray-800">{get(stashAccount, 'meta.name', '')}</h3>
+						<span className="text-gray-500 text-xs">Balance: {get(freeAmount, 'currency', 0)} KSM</span>
+					</div>
+				)}
+
+				<button hidden className="rounded-full border border-gray-300 p-2 px-4 font-semibold text-gray-800">
+					<ChevronRight />
 				</button>
 			</div>
 		</div>
