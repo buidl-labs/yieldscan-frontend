71ed7231296575aef3f7a2fdacf51ad40a35700b
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-26 14:41:19 +0530

    feat: 🎸 advanced validator-selection now has dynamic values

--- components/validators/ValidatorsResult.js
+++ components/validators/ValidatorsResult.js
@@ -1,6 +1,20 @@
 import { Edit2 } from "react-feather";
+import { get } from "lodash";
 
-const ValidatorsResult = () => {
+const ValidatorsResult = ({ transactionState, bondedAmount }) => {
+	const {
+		returns,
+		stakingAmount,
+		timePeriodValue,
+		timePeriodUnit,
+		yieldPercentage,
+	} = transactionState;
+
+	const estimatedPortfolio = {
+		currency: Number(returns.currency + stakingAmount + get(bondedAmount, 'currency', 0)).toFixed(3),
+		subCurrency: Number(returns.currency + stakingAmount + get(bondedAmount, 'subCurrency', 0)).toFixed(3),
+	};
+
 	return (
 		<div className="flex justify-around items-center">
 			<h1 className="text-3xl">Validators</h1>
@@ -8,37 +22,41 @@ const ValidatorsResult = () => {
 				<div className="flex flex-col px-3 py-1 border rounded-lg mr-2 h-16">
 					<span className="text-sm text-teal-500">Staking Amount</span>
 					<h3 className="flex justify-between items-center text-xl">
-						<span className="mr-5">3000 KSM</span>
+						<span className="mr-5">{stakingAmount} KSM</span>
 						<Edit2 size="20px" strokeWidth="2px" className="mb-1 cursor-pointer" />
 					</h3>
-					<span hidden className="text-gray-600 text-xs">$1500</span>
+					<span hidden className="text-gray-600 text-xs">${stakingAmount * 2}</span>
 				</div>
 				<div className="flex flex-col px-3 py-1 border rounded-lg mr-2 h-16">
 					<span className="text-sm text-teal-500">Time Period</span>
 					<h3 className="flex justify-between items-center text-xl">
-						<span className="mr-5">6 months</span>
+						<span className="mr-5">{timePeriodValue} {timePeriodUnit}</span>
 						<Edit2 size="20px" strokeWidth="2px" className="mb-1 cursor-pointer" />
 					</h3>
 				</div>
 				<div className="flex flex-col px-3 py-1 border rounded-lg mr-2 h-16">
 					<span className="text-sm text-teal-500">Expected Yield</span>
 					<h3 className="flex items-center text-xl">
-						<span className="mr-2">5.46%</span>
+						<span className="mr-2">{yieldPercentage}%</span>
 					</h3>
 				</div>
 				<div className="flex flex-col px-3 py-1 border rounded-lg mr-2 h-16">
 					<span className="text-sm text-teal-500">Estimated Portfolio Value</span>
 					<h3 className="flex items-center text-xl">
-						<span className="mr-2">1000 KSM</span>
+						<span className="mr-2">
+							{estimatedPortfolio.currency} KSM
+						</span>
 					</h3>
-					<span hidden className="text-gray-600 text-xs">$500</span>
+					<span hidden className="text-gray-600 text-xs">
+						${estimatedPortfolio.subCurrency}
+					</span>
 				</div>
 				<div className="flex flex-col px-3 py-1 bg-teal-500 text-white rounded-lg h-16">
 					<span className="text-sm">Expected Returns</span>
 					<h3 className="flex items-center text-xl">
-						<span className="mr-2">100 KSM</span>
+						<span className="mr-2">{returns.currency} KSM</span>
 					</h3>
-					<span hidden className="text-gray-600 text-xs">$50</span>
+					<span hidden className="text-gray-600 text-xs">${returns.subCurrency}</span>
 				</div>
 			</div>
 		</div>
--- components/validators/index.js
+++ components/validators/index.js
@@ -1,23 +1,33 @@
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useTransaction, useAccounts } from "@lib/store";
 import { useDisclosure } from "@chakra-ui/core";
 import ValidatorsResult from "./ValidatorsResult";
 import ValidatorsTable from "./ValidatorsTable";
 import EditAmountModal from "./EditAmountModal";
 
 const Validators = () => {
-	const router = useRouter();
+	const { bondedAmount } = useAccounts();
 	const { isOpen, onClose } = useDisclosure();
+	const transactionState = useTransaction();
 
 	useEffect(() => {
-		console.log(router.query);
-	}, [router.query]);
+		console.log(transactionState);
+	}, [transactionState]);
 
 	return (
 		<div className="px-10 py-5">
-			<EditAmountModal isOpen={isOpen} onClose={onClose} />
-			<ValidatorsResult />
-			<ValidatorsTable />
+			<EditAmountModal
+				isOpen={isOpen}
+				onClose={onClose}
+			/>
+			<ValidatorsResult
+				bondedAmount={bondedAmount}
+				transactionState={transactionState}
+			/>
+			<ValidatorsTable
+				validatorMap={transactionState.validatorMap}
+				selectedValidators={transactionState.selectedValidators}
+			/>
 		</div>
 	);
 }
