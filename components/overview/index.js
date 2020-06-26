3c23690a4c668a6ef4058061ce7e6d2c371bc5d9
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-26 10:56:51 +0530

    feat: 🎸 validator-table header design implemented

--- components/overview/index.js
+++ components/overview/index.js
@@ -1,9 +1,22 @@
 import OverviewCards from "./OverviewCards";
+import { Edit2 } from "react-feather";
 
 const Overview = () => {
 	return (
 		<div className="px-10 py-10">
 			<OverviewCards />
+			<div>
+				<div className="flex justify-between items-center mt-10">
+					<h3 className="text-2xl">My Validators</h3>
+					<div className="flex items-center">
+						<button className="flex items-center text-gray-500 mr-5 p-1">
+							<Edit2 size="20px" className="mr-2" />
+							<span>Edit Validators</span>
+						</button>
+						<button className="text-teal-500 p-1">Claim All Rewards</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
