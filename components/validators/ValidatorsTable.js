b77d8645ee2405b97020b8e6bdb81dd754f3207e
Akshat <akshatbhargavadeepa@gmail.com>
2020-06-26 06:01:53 +0530

    feat: 🎸 added selected style in validators table-card

--- components/validators/ValidatorsTable.js
+++ components/validators/ValidatorsTable.js
@@ -1,15 +1,20 @@
 import { Check } from "react-feather";
 import RiskTag from "@components/reward-calculator/RiskTag";
 
-const ValidatorCard = () => (
-	<div className="flex justify-around items-center border-2 border-gray-300 py-2 my-2 rounded-lg">
+const ValidatorCard = ({ selected }) => (
+	<div
+		className={`
+			flex justify-around items-center border-2 py-2 my-2 rounded-lg cursor-pointer
+			${selected ? 'border-teal-500' : 'border-gray-300'}
+		`}
+	>
 		<Check
 			size="1.75rem"
-			className="p-1 bg-teal-500 text-white mr-2 rounded-full" 
+			className={`p-1 mr-2 rounded-full text-white ${selected ? 'bg-teal-500' : 'bg-gray-500'}`}
 			strokeWidth="4px"
 		/>
-		<img src="http://placehold.it/255" className="rounded-full w-12 h-12 mr-4" />
-		<h3 className="text-gray-700">{'abc'}</h3>
+		<img src="http://placehold.it/255" className="rounded-full w-10 h-10 mr-4" />
+		<h3 className="text-gray-700 text-xs">DasX1jVCRMe5e2DN8XJjGQrWFkBkNvtRDRf5dkb6iUUGFfz</h3>
 		<div className="flex flex-col">
 			<span className="text-xs text-gray-500 font-semibold">Risk Score</span>
 			<div className="rounded-full font-semibold"><RiskTag risk={55} /></div>
@@ -38,7 +43,7 @@ const ValidatorsTable = () => {
 		<div>
 			<div className="my-5 table-container overflow-y-scroll">
 				<ValidatorCard />
-				<ValidatorCard />
+				<ValidatorCard selected />
 				<ValidatorCard />
 			</div>
 			<style jsx>{`
