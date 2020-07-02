2488b83acde19eef27a48b3fd83969fc4f9dd4a4
Akshat <akshatbhargavadeepa@gmail.com>
2020-07-02 13:22:57 +0530

    feat: 🎸 added validator-list data fetching and loading ui

--- components/overview/FundsUpdate.js
+++ components/overview/FundsUpdate.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
 	Modal,
 	ModalOverlay,
@@ -9,6 +10,7 @@ import {
 	useToast,
 	Input
 } from "@chakra-ui/core";
+import axios from "@lib/axios";
 import withSlideIn from "@components/common/withSlideIn";
 import RiskTag from "@components/reward-calculator/RiskTag";
 
@@ -39,6 +41,14 @@ const ValidatorCard = ({
 const FundsUpdate = withSlideIn(({ styles, type, close }) => {
 	const title = `${type === 'bond' ? 'Bond Additional' : 'Unbond'} Funds`;
 	const [validatorsLoading, setValidatorsLoading] = useState(true);
+	const [validators, setValidators] = useState([]);
+	useEffect(() => {
+		axios.get('/rewards/risk-set').then(({ data }) => {
+			const validators = data.lowriskset; // pick the low risk validator set automatically
+			setValidators(validators);
+			setValidatorsLoading(false);
+		});
+	}, []);
 
 	return (
 		<Modal isOpen={true} onClose={close} isCentered>
@@ -49,44 +59,51 @@ const FundsUpdate = withSlideIn(({ styles, type, close }) => {
 				</ModalHeader>
 				<ModalCloseButton onClick={close} />
 				<ModalBody px="4rem">
-					<div className="flex justify-around">
-						<div className="border border-gray-200 p-10 rounded-lg text-gray-800 w-1/3">
-							<div>
-								<h3 className="text-xl">Currently Bonded</h3>
-								<h1 className="text-3xl">700 KSM</h1>
-								<span className="text-lg text-gray-600">$350</span>
-							</div>
-							<div className="mt-10">
-								<h3>{title}</h3>
-								<div className="border border-gray-200 rounded-lg">
-									<input
-										className="rounded outline-none p-2 text-xl text-teal-500 rounded-lg"
-									/>
-								</div>
-							</div>
-							<div className="mt-10">
-								<h3 className="text-xl">Total Staking Amount</h3>
-								<h1 className="text-3xl">1000 KSM</h1>
-								<span className="text-lg text-gray-600">$500</span>
-							</div>
+					{validatorsLoading ? (
+						<div className="flex flex-col items-center justify-center mt-40">
+							<Spinner size="lg" />
+							<span className="mt-5 text-sm text-gray-600">Quick deep breath...</span>
 						</div>
-						
-						<div className="border border-gray-400 rounded-lg w-2/3">
-							<div className="flex justify-between items-center px-4 py-2 text-gray-700">
-								<h3 className="text-lg font-semibold">VALIDATORS</h3>
-								<div className="flex items-center">
-									<span className="mr-2 text-sm">Estimated Monthly Returns</span>
-									<div className="py-1 px-2 flex flex-col rounded-lg border border-teal-500 w-24">
-										<h3 className="text-teal-500">460 KSM</h3>
-										<span hidden className="text-gray-600 text-sm">$120</span>
+					) : (
+						<div className="flex justify-around">
+							<div className="border border-gray-200 p-10 rounded-lg text-gray-800 w-1/3">
+								<div>
+									<h3 className="text-xl">Currently Bonded</h3>
+									<h1 className="text-3xl">700 KSM</h1>
+									<span className="text-lg text-gray-600">$350</span>
+								</div>
+								<div className="mt-10">
+									<h3>{title}</h3>
+									<div className="border border-gray-200 rounded-lg">
+										<input
+											className="rounded outline-none p-2 text-xl text-teal-500 rounded-lg"
+										/>
 									</div>
 								</div>
+								<div className="mt-10">
+									<h3 className="text-xl">Total Staking Amount</h3>
+									<h1 className="text-3xl">1000 KSM</h1>
+									<span className="text-lg text-gray-600">$500</span>
+								</div>
 							</div>
-							<div className="overflow-y-scroll">
+							
+							<div className="border border-gray-400 rounded-lg w-2/3">
+								<div className="flex justify-between items-center px-4 py-2 text-gray-700">
+									<h3 className="text-lg font-semibold">VALIDATORS</h3>
+									<div className="flex items-center">
+										<span className="mr-2 text-sm">Estimated Monthly Returns</span>
+										<div className="py-1 px-2 flex flex-col rounded-lg border border-teal-500 w-24">
+											<h3 className="text-teal-500">460 KSM</h3>
+											<span hidden className="text-gray-600 text-sm">$120</span>
+										</div>
+									</div>
+								</div>
+								<div className="overflow-y-scroll">
 
+								</div>
 							</div>
 						</div>
-					</div>
+					)}
 				</ModalBody>
 			</ModalContent>
 		</Modal>
