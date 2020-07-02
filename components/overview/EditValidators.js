646e7a3ef248cb86ac10e5f1798e8290c59c16ed
Akshat <akshatbhargavadeepa@gmail.com>
2020-07-03 05:26:40 +0530

    feat: 🎸 implemented validator-card ui with hover effect

--- components/overview/EditValidators.js
+++ components/overview/EditValidators.js
@@ -10,10 +10,43 @@ import {
 	useToast,
 	Input
 } from "@chakra-ui/core";
-import { CheckCircle, PlusCircle } from "react-feather";
+import { CheckCircle, PlusCircle, MinusCircle } from "react-feather";
 import useHover from "@components/common/hooks/useHover";
 import { useState, useEffect } from "react";
 import axios from "@lib/axios";
+import RiskTag from "@components/reward-calculator/RiskTag";
+
+const ValidatorCard = ({
+	stashId,
+	riskScore,
+	stakedAmount,
+	estimatedReward,
+	isSelected = false,
+}) => {
+	const [ref, hovered] = useHover();
+	return (
+		<div ref={ref} className="relative bg-white flex justify-around items-center py-2 my-2 rounded-lg border border-gray-300">
+			<img src="http://placehold.it/255" className="rounded-full w-10 h-10 mr-4" />
+			<h3 className="text-gray-700 text-xs w-48 truncate">{stashId}</h3>
+			<div className="flex flex-col">
+				<span className="text-xs text-gray-500 font-semibold">Risk Score</span>
+				<div className="rounded-full font-semibold"><RiskTag risk={Number(riskScore.toFixed(2))} /></div>
+			</div>
+			<div className="flex flex-col">
+				<span className="text-xs text-gray-500 font-semibold">Staked Amount</span>
+				<h3 className="text-lg">{stakedAmount.toFixed(1)} KSM</h3>
+			</div>
+			<div className="flex flex-col">
+				<span className="text-xs text-gray-500 font-semibold">Estimated Reward</span>
+				<h3 className="text-lg">{estimatedReward.toFixed(4)} KSM</h3>
+			</div>
+			<div hidden={!hovered} className="absolute bg-white bg-opacity-25 cursor-pointer right-0 px-4 py-2">
+				{!isSelected && <PlusCircle size="2rem" fill="#2BCACA" className="text-white" />}
+				{isSelected && <MinusCircle size="2rem" fill="#e53e3e" className="text-white" />}
+			</div>
+		</div>
+	);
+};
 
 const EditValidators = withSlideIn(({ styles, close }) => {
 	const [validators, setValidators] = useState([]);
@@ -47,8 +80,17 @@ const EditValidators = withSlideIn(({ styles, close }) => {
 								<div className="">
 									<h3 className="font-semibold">CANDIDATE VALIDATORS</h3>
 								</div>
-								<div>
-									validators card here
+								<div className="my-2 overflow-y-scroll validators-table">
+									{validators.map(validator => (
+										<ValidatorCard
+											key={validator.stashId}
+											stashId={validator.stashId}
+											riskScore={validator.riskScore}
+											estimatedReward={validator.estimatedPoolReward}
+											stakedAmount={validator.totalStake}
+											type='add'
+										/>
+									))}
 								</div>
 							</div>
 							<div className="w-1/2 border border-gray-300 p-2 rounded-lg">
@@ -72,6 +114,11 @@ const EditValidators = withSlideIn(({ styles, close }) => {
 							</div>
 						</div>
 					)}
+					<style jsx>{`
+						.validators-table {
+							height: 62vh;
+						}
+					`}</style>
 				</ModalBody>
 			</ModalContent>
 		</Modal>
