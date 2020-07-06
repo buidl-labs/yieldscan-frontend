e2a2422101ebd6fa15d5b677822d59cd5cc9dac3
Akshat <akshatbhargavadeepa@gmail.com>
2020-07-06 09:44:28 +0530

    feat: 🎸 update: profile edit design, now looks minimal <3

--- components/validator-profile/EditProfileModal.js
+++ components/validator-profile/EditProfileModal.js
@@ -45,22 +45,22 @@ const EditProfileModal = withSlideIn(({ styles, onClose }) => {
 	return (
 		<Modal isOpen={true} onClose={onClose} isCentered>
 			<ModalOverlay />
-			<ModalContent rounded="lg" maxWidth="70vw" height="90vh" {...styles}>
+			<ModalContent rounded="lg" maxWidth="70vw" height="85vh" {...styles}>
 				<ModalHeader>
-					<h3 className="text-gray-700 font-normal text-2xl">Manage Profile</h3>
+					<h3 className="text-gray-700 font-semibold text-xs">Manage Profile</h3>
+					<div className="flex">
+					{false && <img src="http://placehold.it/300" className="w-16 h-16 mr-5 rounded-full" />}
+					<div className="flex flex-col">
+						<h3 className="text-2xl text-gray-700 font-semibold">{'Validator Name'}</h3>
+						<p className="text-gray-600 text-sm">5D5FrhPca4LiKzCmbRiFqK3rMfTwwi7hxqaugyUdcvFyX13p</p>
+					</div>
+				</div>
 				</ModalHeader>
 				<ModalCloseButton onClick={onClose} />
 				<ModalBody overflowY="scroll">
-					<div className="mt-2 p-4 rounded-lg border border-gray-300">
-						<div className="flex">
-							<img src="http://placehold.it/300" className="w-16 h-16 mr-5 rounded-full" />
-							<div className="flex flex-col">
-								<h3 className="text-2xl text-gray-700 font-semibold">{'Validator Name'}</h3>
-								<p className="text-gray-600 text-sm">5D5FrhPca4LiKzCmbRiFqK3rMfTwwi7hxqaugyUdcvFyX13p</p>
-							</div>
-						</div>
-						<div className="mt-5">
-							<h5 className="text-gray-800 mb-2">Description</h5>
+					<div className="px-4">
+						<div>
+							<h5 className="text-gray-600 mb-2 font-bold text-lg">Description</h5>
 							<textarea
 								rows={4}
 								className="w-full p-2 outline-none border border-gray-400 text-gray-600 rounded-lg"
@@ -68,7 +68,7 @@ const EditProfileModal = withSlideIn(({ styles, onClose }) => {
 							/>
 						</div>
 						<div className="mt-5">
-							<h5 className="text-gray-800 mb-2">Team Members</h5>
+							<h5 className="text-gray-600 mb-2 font-bold text-lg">Team Members</h5>
 							<div className="my-4">
 								<MemberInput />
 							</div>
@@ -81,6 +81,20 @@ const EditProfileModal = withSlideIn(({ styles, onClose }) => {
 							<div className="my-4">
 								<MemberInput newMember />
 							</div>
+						</div>
+						<div className="flex-center">
+							<Button
+								px="8"
+								py="2"
+								mt="5"
+								rounded="0.5rem"
+								backgroundColor="teal.500"
+								color="white"
+								onClick={() => {}}
+								isLoading={false}
+							>
+								Update Profile
+							</Button>
 						</div>
 					</div>
 				</ModalBody>
