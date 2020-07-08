01593c44f8add470ed4325d428601199002ecc7f
Akshat <akshatbhargavadeepa@gmail.com>
2020-07-08 20:21:37 +0530

    feat: 🎸 added error state to council-members table

--- components/governance/index.js
+++ components/governance/index.js
@@ -1,16 +1,30 @@
+import axios from "@lib/axios";
+import { Spinner } from "@chakra-ui/core";
 import { useState, useEffect } from "react";
 import MembersTable from "./MembersTable";
-import axios from "@lib/axios";
 
 const Governance = () => {
 	const [members, setMembers] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		axios.get('/council/members').then(({ data }) => {
 			setMembers(data.members);
+			setLoading(false);
 		});
 	}, []);
 	
+	if (loading) {
+		return (
+			<div className="flex-center w-full h-full">
+				<div className="flex-center flex-col">
+					<Spinner size="xl" />
+					<span className="text-sm text-gray-600 mt-5">Fetching council members...</span>
+				</div>
+			</div>
+		);
+	}
+
 	return (
 		<div  className="px-10 py-10">
 			<h1 className="text-2xl text-gray-600 font-semibold">Council Members</h1>
