8f9691c7c370cf5ba6cc83bc8841694e98b0209f
Akshat <akshatbhargavadeepa@gmail.com>
2020-07-08 22:57:52 +0530

    feat: 🎸 nominators page stats card developed

--- components/nominators/index.js
+++ components/nominators/index.js
@@ -15,8 +15,19 @@ const Nominators = () => {
 					</div>
 					<NominationsTable />
 				</div>
-				<div className="w-1/3 flex flex-col">
-					<h1>cards here</h1>
+				<div className="w-1/3 flex flex-col justify-around">
+					<div className="shadow-xl flex flex-col rounded-lg pt-16 pb-10 px-12 text-white" style={{ background: '#1F495B' }}>
+						<h1 className="text-4xl">1245</h1>
+						<h3 className="text-lg">Active Nominations</h3>
+					</div>
+					<div className="shadow-xl flex flex-col rounded-lg pt-16 pb-10 px-12 text-white" style={{ background: '#1F495B' }}>
+						<h1 className="text-4xl">1420 KSM</h1>
+						<h3 className="text-lg">Total Amount Staked</h3>
+					</div>
+					<div className="shadow-xl flex flex-col rounded-lg pt-16 pb-10 px-12 bg-teal-500 text-white">
+						<h1 className="text-4xl">1200 KSM</h1>
+						<h3 className="text-lg">Total Rewards</h3>
+					</div>
 				</div>
 			</div>
 		</div>
