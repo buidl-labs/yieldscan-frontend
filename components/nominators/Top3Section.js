010b018d139dc2f0db4af7bdc3d3db56a5741d78
Akshat <akshatbhargavadeepa@gmail.com>
2020-07-09 05:29:08 +0530

    feat: 🎸 nominators top-3 section dynamic now

--- components/nominators/Top3Section.js
+++ components/nominators/Top3Section.js
@@ -1,22 +1,32 @@
 import { Avatar } from "@chakra-ui/core";
 
-const TopNominatorCard = () => (
+const TopNominatorCard = ({ stashId, nominations, dailyEarnings }) => (
 	<div className="px-6 py-8 flex-center flex-col rounded-lg border shadow-lg">
 		<Avatar mb="1rem" />
-		<h3 className="text-xl text-gray-700 font-semibold mb-4">Nominator Name</h3>
-		<span className="text-gray-600 font-semibold text-sm">Daily Earning</span>
-		<h5 className="font-semibold">10k KSM</h5>
+		<h3 className="select-all text-gray-700 font-semibold mb-4 text-xs truncate w-40">{stashId}</h3>
+		<span className="text-gray-600 font-semibold text-sm">Daily Earnings</span>
+		<h5 className="font-semibold">{dailyEarnings.toFixed(2)} KSM</h5>
 		<span className="text-gray-600 font-semibold text-sm">Nominations</span>
-		<h5 className="font-semibold">42</h5>
+		<h5 className="font-semibold">{nominations}</h5>
 	</div>
 );
 
-const Top3Section = () => {
+const Top3Section = ({ nominators = [] }) => {
 	return (
 		<div className="flex items-center">
-			<div className="mr-5"><TopNominatorCard /></div>
-			<div className="mx-5"><TopNominatorCard /></div>
-			<div className="mx-5"><TopNominatorCard /></div>
+			{nominators.map(nominator => (
+				<div
+					key={nominator.nomId}
+					className="mr-5"
+				>
+					<TopNominatorCard
+						stashId={nominator.nomId}
+						nominations={nominator.nominations}
+						totalStake={nominator.nomtotalStake}
+						dailyEarnings={nominator.dailyEarnings}
+					/>
+				</div>
+			))}
 		</div>
 	);
 };
