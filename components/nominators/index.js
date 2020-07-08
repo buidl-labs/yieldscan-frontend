ca2865d50236bd0ca765bdaecb7ca623a78d9319
Akshat <akshatbhargavadeepa@gmail.com>
2020-07-09 05:23:42 +0530

    feat: 🎸 integrrated nominators API with loading and error state

--- components/nominators/index.js
+++ components/nominators/index.js
@@ -1,8 +1,44 @@
-import { Avatar } from "@chakra-ui/core";
+import { Avatar, Spinner } from "@chakra-ui/core";
 import Top3Section from "./Top3Section";
 import NominationsTable from "./NominatorsTable";
+import { useEffect, useState } from "react";
+import axios from "@lib/axios";
 
 const Nominators = () => {
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [nominatorsData, setNominatorsData] = useState([]);
+
+	useEffect(() => {
+		setLoading(true);
+		setError(false);
+		axios.get('/actors/nominators').then(({ data }) => {
+			setNominatorsData(data);
+		}).catch(() => {
+			setError(true);
+		}).finally(() => {
+			setLoading(false);
+		});
+	}, []);
+	
+	if (loading) {
+		return (
+			<div className="flex-center flex-col mt-40">
+				<Spinner className="text-gray-700 mb-2" />
+				<span className="text-gray-600 text-sm">Fetching Nominators...</span>
+			</div>
+		);
+	}
+
+	if (error) {
+		return (
+			<div className="flex-center flex-col mt-40">
+				<div className="text-4xl">🧐</div>
+				<h3>Sorry, something went wrong while fetching! We'll surely look into this.</h3>
+			</div>
+		);
+	}
+
 	return (
 		<div className="px-10 py-5">
 			<h1 className="text-2xl text-gray-800 font-semibold">Nominators</h1>
@@ -11,7 +47,7 @@ const Nominators = () => {
 				<div className="w-2/3 mr-4">
 					<div className="mt-10">
 						<p className="text-gray-600 font-thin mb-2">TOP NOMINATORS</p>
-						<Top3Section />
+						<Top3Section nominators={nominatorsData.top3.slice(0, 3)} />
 					</div>
 					<NominationsTable />
 				</div>
