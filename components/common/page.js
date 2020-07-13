f427da5eff11550a3680f897b3a46d23f506de6f
Akshat <akshatbhargavadeepa@gmail.com>
2020-07-13 11:24:47 +0530

    feat: 🎸 added view for mobile screen handling

--- components/common/page.js
+++ components/common/page.js
@@ -1,7 +1,19 @@
 import Head from 'next/head';
+import { isMobile, isTablet } from 'react-device-detect';
 
 const Page = ({ title, children, layoutProvider }) => {
 	const layoutedChild = layoutProvider ? layoutProvider(children) : children;
+
+	if (isMobile || isTablet) {
+		return (
+			<div className="flex-center flex-col bg-gray-100" style={{ width: '100vw', height: '100vh' }}>
+				<span className="text-lg text-teal-500 font-bold text-4xl mb-10">YieldScan</span>
+				<h1 className="text-xl text-center text-gray-800 font-semibold px-5">
+					We don't support this device yet. To use YieldScan please visit us on your desktop / laptop.
+				</h1>
+			</div>
+		);
+	}
 
 	return (
 		<div>
