31c782ef856fd7efbbcfcf2499553298cf68870c
akshat <akshatbhargavadeepa@gmail.com>
2020-08-09 21:40:54 +0530

    feat: 🎸 added task-card design in TS-modal

--- components/validator-profile/TransparencyScoreModal.js
+++ components/validator-profile/TransparencyScoreModal.js
@@ -10,14 +10,27 @@ import {
 import withSlideIn from "@components/common/withSlideIn";
 import ProfileBadge from "@components/common/ProfileBadge";
 
+const TaskCard = ({ title = '', description = '', score = 0 }) => (
+  <div className="p-5 px-10 my-5 flex items-center justify-between border border-gray-300 rounded-xl">
+    <div>
+      <h1 className="text-xl font-semibold">{title}</h1>
+      <h3 className="text-lg text-gray-600">{description}</h3>
+    </div>
+    <div className="flex flex-col items-center">
+      <span className="font-semibold text-3xl text-teal-500">{score}</span>
+      <span className="font-semibold text-lg">PTS</span>
+    </div>
+  </div>
+);
+
 const TransparencyScoreModal = withSlideIn(({ onClose, styles }) => {
   return (
 		<Modal isOpen={true} onClose={onClose} isCentered>
 			<ModalOverlay />
-			<ModalContent rounded="lg" maxWidth="70vw" height="85vh" {...styles}>
+			<ModalContent rounded="lg" maxWidth="70vw" {...styles}>
 				<ModalHeader>
           <div className="flex justify-between items-center mr-20 select-none">
-					  <h1 className="px-4 text-gray-700 text-4xl font-bold">Transparency Score</h1>
+					  <h1 className="px-10 text-gray-700 text-3xl font-bold">Transparency Score</h1>
             <div className="flex items-center">
               <div className="mr-4"><ProfileBadge score={10} /></div>
               <div className="flex flex-col">
@@ -29,10 +42,41 @@ const TransparencyScoreModal = withSlideIn(({ onClose, styles }) => {
 				</ModalHeader>
 				<ModalCloseButton onClick={onClose} />
 				<ModalBody overflowY="scroll">
-					<div className="px-4">
-            <p className="text-gray-600">Completing the following tasks can boost your profile and increase your transparency score</p>
-            <div>
+					<div className="px-10 pb-16 pt-5">
+            <p className="text-gray-600">Completing the following tasks can boost your profile and increase your transparency score:</p>
+            <div className="mt-10 flex items-center justify-between">
+              <h3 className="text-xl font-semibold">TASKS</h3>
+              <div>
+                <span className="font-semibold text-lg">0/7 Tasks Completed</span>
+              </div>
+            </div>
 
+            <div className="overflow-y-scroll" style={{ maxHeight: 500 }}>
+              <TaskCard
+                title="Add your display name"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+                score={10}
+              />
+              <TaskCard
+                title="Add your legal name"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+                score={10}
+              />
+              <TaskCard
+                title="Add your e-mail"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+                score={10}
+              />
+            </div>
+
+            <div className="mt-5 flex items-center justify-center">
+              <Button
+                size="lg"
+                leftIcon="repeat"
+                variantColor="teal"
+              >
+                Refresh
+              </Button>
             </div>
 					</div>
 				</ModalBody>
