37156a65b57b86a7b3f2dfd0e37fce763e12a13f
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2020-08-18 13:53:54 +0530

    🛠nominators tab ui fixes

--- components/nominators/Top3Section.js
+++ components/nominators/Top3Section.js
@@ -1,33 +1,54 @@
 import Identicon from "@components/common/Identicon";
 import { ExternalLink } from "react-feather";
 
-const TopNominatorCard = ({ name, stashId, nominations, dailyEarnings, onProfile }) => (
-	<div className="px-6 py-8 flex-center flex-col rounded-lg border shadow-lg">
-		<Identicon address={stashId} size="3rem" />
+const TopNominatorCard = ({
+	name,
+	stashId,
+	nominations,
+	totalStake,
+	dailyEarnings,
+	onProfile,
+}) => (
+	<div className="mt-4 px-10 py-8 flex-center flex-col border border-gray-200 rounded-lg shadow-lg">
+		<Identicon address={stashId} size="3.5rem" />
 		<div
-			className="cursor-pointer text-center text-gray-700 font-semibold mb-4 text-xs truncate w-40"
+			className="cursor-pointer text-center text-gray-900 mt-4 truncate w-40"
 			onClick={onProfile}
 		>
-			<span>{name || stashId || '-'}</span>
-			<div className="flex justify-center items-center text-gray-700" onClick={onProfile}>
+			<span className="text-base font-black">
+				{name || stashId.slice(0, 6) + "..." + stashId.slice(-6) || "-"}
+			</span>
+			<div
+				className="flex justify-center items-center text-gray-700"
+				onClick={onProfile}
+			>
 				<span className="text-xs mr-1">View on Polkascan</span>
 				<ExternalLink size="12px" />
 			</div>
 		</div>
-		<span className="text-gray-600 font-semibold text-sm">Daily Earnings</span>
-		<h5 className="font-semibold">{(dailyEarnings || 0).toFixed(2)} KSM</h5>
-		<span className="text-gray-600 font-semibold text-sm">Nominations</span>
-		<h5 className="font-semibold">{nominations}</h5>
+		<span className="text-gray-600 font-semibold text-xs mt-6">
+			Daily Earnings
+		</span>
+		<h5 className="font-semibold text-base">
+			{(dailyEarnings || 0).toFixed(2)} KSM
+		</h5>
+		<span className="text-gray-600 font-semibold text-xs mt-4">
+			Total Amount Staked
+		</span>
+		<h5 className="font-semibold text-base">{totalStake}</h5>
+		<span className="text-gray-600 font-semibold text-xs mt-4">
+			Nominations
+		</span>
+		<h5 className="font-semibold text-base">{nominations}</h5>
 	</div>
 );
 
 const Top3Section = ({ nominators = [] }) => {
 	return (
-		<div className="flex items-center">
+		<div className="flex items-center justify-between">
 			{nominators.map(nominator => (
 				<div
 					key={nominator.nomId}
-					className="mr-5"
 				>
 					<TopNominatorCard
 						name={nominator.name}
--- components/nominators/index.js
+++ components/nominators/index.js
@@ -44,24 +44,36 @@ const Nominators = () => {
 			<h1 className="text-2xl text-gray-800 font-semibold">Nominators</h1>
 
 			<div className="flex w-full">
-				<div className="w-2/3 mr-4">
-					<div className="mt-10">
-						<p className="text-gray-600 font-thin mb-2">TOP NOMINATORS</p>
+				<div className="w-2/3">
+					<div className="mt-6">
+						<p className="text-gray-600 text-xs mb-2 tracking-widest">
+							TOP NOMINATORS
+						</p>
 						<Top3Section nominators={nominatorsData.top3.slice(0, 3)} />
 					</div>
 					<NominationsTable nominators={nominatorsData.nominatorsInfo} />
 				</div>
-				<div className="w-1/3 flex flex-col justify-around">
-					<div className="shadow-xl flex flex-col rounded-lg pt-16 pb-10 px-12 text-white" style={{ background: '#1F495B' }}>
+				<div className="sticky top-0 self-start pt-8 ml-8">
+					<div
+						className="shadow-xl flex flex-col rounded-lg pt-16 pb-10 px-12 text-white"
+						style={{ background: "#1F495B" }}
+					>
 						<h1 className="text-4xl">{nominatorsData.stats.nominatorsCount}</h1>
 						<h3 className="text-lg">Active Nominators</h3>
 					</div>
-					<div className="shadow-xl flex flex-col rounded-lg pt-16 pb-10 px-12 text-white" style={{ background: '#1F495B' }}>
-						<h1 className="text-4xl">{Math.trunc(nominatorsData.stats.totalAmountStaked)} KSM</h1>
+					<div
+						className="shadow-xl flex flex-col rounded-lg my-10 pt-16 pb-10 px-12 text-white"
+						style={{ background: "#1F495B" }}
+					>
+						<h1 className="text-4xl">
+							{Math.trunc(nominatorsData.stats.totalAmountStaked)} KSM
+						</h1>
 						<h3 className="text-lg">Total Amount Staked</h3>
 					</div>
 					<div className="shadow-xl flex flex-col rounded-lg pt-16 pb-10 px-12 bg-teal-500 text-white">
-						<h1 className="text-4xl">{Math.trunc(nominatorsData.stats.totalRewards)} KSM</h1>
+						<h1 className="text-4xl">
+							{Math.trunc(nominatorsData.stats.totalRewards)} KSM
+						</h1>
 						<h3 className="text-lg">Total Rewards</h3>
 					</div>
 				</div>
