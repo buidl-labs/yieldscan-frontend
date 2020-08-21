941b3af26f83becfc2e1445c727853db7c8bb565
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2020-08-21 13:23:32 +0530

    Feature/sentry addition (#80)
    * 🛠UI fixes + improvements
    
    * 🛠more UI fixes
    
    * 🛠header ui fixes
    
    * 🛠 payment ui fixes
    
    * 🛠header account popover ui fix
    
    * 🛠Edit controller modal ui fix
    
    * 🛠result card insight popover ui fix
    
    * 🛠ui fixes for calculator
    
    * 🛠connector position fix
    
    * 🏗[WIP] Calculator fixes
    
    * 🏗[WIP] Calculator state fix
    
    * 🛠nominators tab ui fixes
    
    * 🛠governance tab UI fix
    
    * 🛠more nominators tab ui fixes
    
    * ✂️truncated total staked amount
    
    * increased padding for top nominators card
    
    * 💵fiat conversion added from coingecko
    
    * 🎉added number formatting + fiat conversion
    
    * 💵added currency formatting
    
    * 🎉added formatting for stash account balance in header + popover close on select
    
    * close popup on network selection
    
    * hotfix
    
    * hotfix for payment UX transaction step
    
    * fixed wrong amount issue in the final step for payment UX
    
    * validator name UI fix on confirmation page
    
    * risk tag spacing fix
    
    * 🚨[WIP] addition of sentry
    
    * added 404 page
    
    * 🎉sentry is working
    
    * added custom error page with cta to go back
    
    * packages updated
    
    * 🗣added user feedback dialog for error reporting
    
    * unstyled font loading issue fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,46 @@
 import { ConsentGate, MetomicProvider } from "@metomic/react";
+import * as Sentry from "@sentry/node";
 
 import { ThemeProvider, theme } from "@chakra-ui/core";
-import '../styles/index.css';
+import "../styles/index.css";
 
 const customTheme = {
-  ...theme,
-  colors: {
-    ...theme.colors,
-    teal: {
-      ...theme.colors.teal,
-      '300': '#45E2E2',
-      '500': '#2BCACA',
-      '700': '#20B1B1',
-    },
-    pink: {
-      ...theme.colors.pink,
-      '300': '#FF9DC0',
-      '500': '#FF7CAB',
-      '700': '#EF6093',
-    },
-    orange: {
-      ...theme.colors.orange,
-      '500': '#F5B100',
-    },
-  },
+	...theme,
+	colors: {
+		...theme.colors,
+		teal: {
+			...theme.colors.teal,
+			"300": "#45E2E2",
+			"500": "#2BCACA",
+			"700": "#20B1B1",
+		},
+		pink: {
+			...theme.colors.pink,
+			"300": "#FF9DC0",
+			"500": "#FF7CAB",
+			"700": "#EF6093",
+		},
+		orange: {
+			...theme.colors.orange,
+			"500": "#F5B100",
+		},
+	},
 };
 
-export default function YieldScanApp({ Component, pageProps }) {
-  return (
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+	Sentry.init({
+    environment: process.env.NODE_ENV,
+		enabled: process.env.NODE_ENV === "production",
+		dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+	});
+}
+
+export default function YieldScanApp({ Component, pageProps, err }) {
+	return (
 		<ThemeProvider theme={customTheme}>
 			<MetomicProvider projectId={process.env.NEXT_PUBLIC_METOMIC_PROJECT_ID}>
-				<Component {...pageProps} />
+				<Component {...pageProps} err={err} />
 			</MetomicProvider>
 		</ThemeProvider>
 	);
-};
+}
