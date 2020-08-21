4014d9a88462d4784efae8302a205a4c84af7a6f
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2020-08-21 13:06:33 +0530

    unstyled font loading issue fix

--- pages/_document.js
+++ pages/_document.js
@@ -13,10 +13,12 @@ class MyDocument extends Document {
 					<link rel="icon" href="/favicon.ico" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-						rel="stylesheet"
+						rel="preload"
+						as="font"
 					/>
 					<link
-						rel="stylesheet"
+						rel="preload"
+						as="font"
 						href="https://cdn.jsdelivr.net/npm/font-proxima-nova@1.0.1/style.css"
 					/>
 					<link
