d3c93d6a6df9609dbf7ca6ab7dde47007f3f0bb8
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2020-08-25 14:33:02 +0530

    view only state UI fix

--- components/validator-profile/ValidatorInfoHeader.js
+++ components/validator-profile/ValidatorInfoHeader.js
@@ -1,8 +1,15 @@
-import { Twitter, Link, ChevronRight, Mail } from "react-feather";
+import { Twitter, Link, ChevronRight, Mail, Eye } from "react-feather";
 import { get, noop } from "lodash";
 import { encodeAddress, decodeAddress } from "@polkadot/util-crypto";
 import Identicon from "@components/common/Identicon";
 import ProfileBadge from "@components/common/ProfileBadge";
+import {
+	Popover,
+	PopoverTrigger,
+	PopoverContent,
+	PopoverArrow,
+	PopoverBody,
+} from "@chakra-ui/core";
 
 const ValidatorInfoHeader = ({
 	stashId,
@@ -32,12 +39,16 @@ const ValidatorInfoHeader = ({
 				<div className="flex justify-between items-center mb-2">
 					<div className="flex items-center">
 						<div>
-							<h3 className="mr-4 text-2xl text-gray-700 font-semibold">
+							<h3 className="mr-4 text-xl text-gray-700 font-semibold">
 								{get(socialInfo, "name") ||
 									stashId.slice(0, 6) + "..." + stashId.slice(-6) ||
 									"-"}
 							</h3>
-							{socialInfo.legal && <p className="text-gray-600">{get(socialInfo, "legal")}</p>}
+							{socialInfo.legal && (
+								<p className="text-gray-600 text-sm">
+									{get(socialInfo, "legal")}
+								</p>
+							)}
 						</div>
 						<ProfileBadge score={transparencyScore.total} />
 					</div>
@@ -79,21 +90,48 @@ const ValidatorInfoHeader = ({
 				</div>
 				<p className="text-gray-500 text-sm mb-2">{vision}</p>
 				<div>
-					{!get(stashAccount, "address") && (
+					{get(stashAccount, "address") ? (
+						userStashKusamaId === stashId ? (
+							<button
+								className="flex items-center text-xs text-gray-700 hover:underline"
+								onClick={openEditProfile}
+							>
+								<span>Edit Profile</span>
+								<ChevronRight size="1rem" className="text-gray-700" />
+							</button>
+						) : (
+							<Popover trigger="hover">
+								<PopoverTrigger>
+									<span className="text-gray-900 text-xs flex items-center cursor-default w-fit-content">
+										<Eye size="0.75rem" />{" "}
+										<span className="ml-1">View Only</span>
+									</span>
+								</PopoverTrigger>
+								<PopoverContent
+									zIndex={4}
+									padding="0"
+									rounded="10px"
+									color="gray.200"
+									textAlign="center"
+									backgroundColor="#35475C"
+									border="none"
+									width="auto"
+								>
+									<PopoverArrow />
+									<PopoverBody fontSize="sm">
+										Your selected address does not match this validator’s
+										address. If you own this profile, then select the same
+										address from the navbar.
+									</PopoverBody>
+								</PopoverContent>
+							</Popover>
+						)
+					) : (
 						<button
 							className="flex items-center text-xs text-gray-700 hover:underline"
 							onClick={toggleWalletConnect}
 						>
 							<span>Own this profile?</span>
-							<ChevronRight size="1rem" className="text-gray-700" />
-						</button>
-					)}
-					{userStashKusamaId === stashId && (
-						<button
-							className="flex items-center text-xs text-gray-700 hover:underline"
-							onClick={openEditProfile}
-						>
-							<span>Edit Profile</span>
 							<ChevronRight size="1rem" className="text-gray-700" />
 						</button>
 					)}
