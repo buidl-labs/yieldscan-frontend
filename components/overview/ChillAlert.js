789477c8c3ae78b9622ee61e0839006a6195c662
Sahil <sahilnanda1995@gmail.com>
2020-09-12 11:24:59 +0530

    feat: 🎸 handled closeOnOverlay click for "stop all nominations"

--- components/overview/ChillAlert.js
+++ components/overview/ChillAlert.js
@@ -9,7 +9,7 @@ import {
 	AlertDialogContent,
 	AlertDialogCloseButton,
 	Button,
-	useToast
+	useToast,
 } from "@chakra-ui/core";
 import chill from "@lib/polkadot/chill";
 import { useAccounts, usePolkadotApi } from "@lib/store";
@@ -20,39 +20,44 @@ const ChillAlert = withSlideIn(({ styles, close }) => {
 	const { stashAccount } = useAccounts();
 	const { apiInstance } = usePolkadotApi();
 	const [chilling, setChilling] = useState(false);
+	const [closeOnOverlayClick, setCloseOnOverlayClick] = useState(true);
 
 	const onConfirm = () => {
 		setChilling(true);
+		setCloseOnOverlayClick(false);
 		chill(stashAccount.address, apiInstance, {
 			onEvent: ({ message }) => {
 				toast({
-					title: 'Info',
+					title: "Info",
 					description: message,
-					status: 'info',
+					status: "info",
 					duration: 3000,
-					position: 'top-right',
+					position: "top-right",
 					isClosable: true,
 				});
 			},
 			onFinish: (failed, message) => {
 				toast({
-					title: failed ? 'Failure' : 'Success',
+					title: failed ? "Failure" : "Success",
 					description: message,
-					status: failed ? 'error' : 'success',
+					status: failed ? "error" : "success",
 					duration: 3000,
-					position: 'top-right',
+					position: "top-right",
 					isClosable: true,
 				});
 				setChilling(false);
+				setCloseOnOverlayClick(true);
 				close();
 			},
-		}).catch(error => {
+		}).catch((error) => {
+			setChilling(false);
+			setCloseOnOverlayClick(true);
 			toast({
-				title: 'Error',
+				title: "Error",
 				description: error.message,
-				status: 'error',
+				status: "error",
 				duration: 3000,
-				position: 'top-right',
+				position: "top-right",
 				isClosable: true,
 			});
 		});
@@ -62,20 +67,28 @@ const ChillAlert = withSlideIn(({ styles, close }) => {
 		<AlertDialog
 			leastDestructiveRef={cancelRef}
 			onClose={close}
+			closeOnOverlayClick={closeOnOverlayClick}
+			closeOnEsc={closeOnOverlayClick}
 			isOpen={true}
 		>
 			<AlertDialogOverlay opacity={styles.opacity} />
 			<AlertDialogContent {...styles}>
 				<AlertDialogHeader>Stop All Nominations?</AlertDialogHeader>
-				<AlertDialogCloseButton hidden={chilling} />
+				{closeOnOverlayClick && <AlertDialogCloseButton hidden={chilling} />}
 				<AlertDialogBody>
-					Are you sure you want to stop all of your current nominations? This will keep your amount bonded in your account.
+					Are you sure you want to stop all of your current nominations? This
+					will keep your amount bonded in your account.
 				</AlertDialogBody>
 				<AlertDialogFooter>
 					<Button ref={cancelRef} onClick={close} isDisabled={chilling}>
 						No, Cancel
 					</Button>
-					<Button variantColor="red" ml={3} onClick={onConfirm} isLoading={chilling}>
+					<Button
+						variantColor="red"
+						ml={3}
+						onClick={onConfirm}
+						isLoading={chilling}
+					>
 						Yes, Confirm
 					</Button>
 				</AlertDialogFooter>
