229f06bd721677ed51841149e92457cd8a46155f
Sahil <sahilnanda1995@gmail.com>
2020-10-16 15:13:12 +0530

    fix: 🐛 fixed council viz network related info

--- components/council-member-profile/council-viz/Circleandline.js
+++ components/council-member-profile/council-viz/Circleandline.js
@@ -2,95 +2,98 @@ import React from "react";
 import { Circle, Line, Text, Rect } from "react-konva";
 
 class Circleandline extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showNominatorAddress: false,
-    };
-  }
-  handleOnMouseOver = (e) => {
-    e.target.setAttrs({
-      scaleX: 1.3,
-      scaleY: 1.3,
-    });
-    document.body.style.cursor = "pointer";
-    this.setState({ showNominatorAddress: true });
-  };
-  handleOnMouseOut = (e) => {
-    e.target.setAttrs({
-      scaleX: 1,
-      scaleY: 1,
-    });
-    document.body.style.cursor = "default";
-    this.setState({ showNominatorAddress: false });
-  };
+	constructor() {
+		super();
+		this.state = {
+			showNominatorAddress: false,
+		};
+	}
+	handleOnMouseOver = (e) => {
+		e.target.setAttrs({
+			scaleX: 1.3,
+			scaleY: 1.3,
+		});
+		document.body.style.cursor = "pointer";
+		this.setState({ showNominatorAddress: true });
+	};
+	handleOnMouseOut = (e) => {
+		e.target.setAttrs({
+			scaleX: 1,
+			scaleY: 1,
+		});
+		document.body.style.cursor = "default";
+		this.setState({ showNominatorAddress: false });
+	};
 
-  render() {
-    const cardWidth = 159;
-    const cardHeight = 69;
+	render() {
+		const cardWidth = 159;
+		const cardHeight = 69;
 
-    let backer = this.props.name !== null ? this.props.name : this.props.backer;
+		let backer = this.props.name !== null ? this.props.name : this.props.backer;
 
-    if (backer.length > 11) {
-      backer = backer.slice(0, 5) + "..." + backer.slice(-5);
-    }
-    let stake = "Stake: " + this.props.stake.toString().slice(0, 7) + " KSM";
+		if (backer.length > 11) {
+			backer = backer.slice(0, 5) + "..." + backer.slice(-5);
+		}
+		let stake =
+			"Stake: " +
+			this.props.stake.toString().slice(0, 7) +
+			` ${this.props.networkInfo.denom}`;
 
-    let x1 = this.props.x + 15;
-    let y1 = this.props.y - 8;
+		let x1 = this.props.x + 15;
+		let y1 = this.props.y - 8;
 
-    if (x1 > this.props.width - cardWidth) {
-      y1 = y1 + 20;
-      x1 = x1 - cardWidth / 2;
-    }
+		if (x1 > this.props.width - cardWidth) {
+			y1 = y1 + 20;
+			x1 = x1 - cardWidth / 2;
+		}
 
-    return (
-      <React.Fragment>
-        <Line
-          points={[this.props.x, this.props.y, this.props.x2, this.props.y2]}
-          stroke="#35475C"
-          strokeWidth={1}
-        />
-        <Circle
-          x={this.props.x}
-          y={this.props.y}
-          radius={7}
-          fill="#2BCACA"
-          onMouseOver={this.handleOnMouseOver}
-          onMouseOut={this.handleOnMouseOut}
-          // onClick={this.handleClick}
-        />
+		return (
+			<React.Fragment>
+				<Line
+					points={[this.props.x, this.props.y, this.props.x2, this.props.y2]}
+					stroke="#35475C"
+					strokeWidth={1}
+				/>
+				<Circle
+					x={this.props.x}
+					y={this.props.y}
+					radius={7}
+					fill="#2BCACA"
+					onMouseOver={this.handleOnMouseOver}
+					onMouseOut={this.handleOnMouseOut}
+					// onClick={this.handleClick}
+				/>
 
-        {this.state.showNominatorAddress && (
-          <Rect
-            x={x1}
-            y={y1}
-            width={cardWidth}
-            height={cardHeight}
-            cornerRadius={4.69457}
-            fill="#48607C"
-            shadowOffsetY={10}
-            shadowBlur={10}
-            shadowColor="black"
-            shadowOpacity={0.5}
-          />
-        )}
+				{this.state.showNominatorAddress && (
+					<Rect
+						x={x1}
+						y={y1}
+						width={cardWidth}
+						height={cardHeight}
+						cornerRadius={4.69457}
+						fill="#48607C"
+						shadowOffsetY={10}
+						shadowBlur={10}
+						shadowColor="black"
+						shadowOpacity={0.5}
+					/>
+				)}
 
-        {this.state.showNominatorAddress && (
-          <Text
-            text={backer}
-            x={x1 + 15}
-            y={y1 + 10}
-            fill="#FFFFFF"
-            fontSize={16}
-            fontStyle={"bold"}
-          />
-        )}
-        {this.state.showNominatorAddress && (
-          <Text text={stake} x={x1 + 15} y={y1 + 35} fill="#B2BECC" />
-        )}
-      </React.Fragment>
-    );
-  }
+				{this.state.showNominatorAddress && (
+					<Text
+						text={backer}
+						x={x1 + 15}
+						y={y1 + 10}
+						fill="#FFFFFF"
+						fontSize={16}
+						fontStyle={"bold"}
+					/>
+				)}
+				{this.state.showNominatorAddress && (
+					<Text text={stake} x={x1 + 15} y={y1 + 35} fill="#B2BECC" />
+				)}
+			</React.Fragment>
+		);
+	}
 }
 export default Circleandline;
--- components/council-member-profile/council-viz/Network.js
+++ components/council-member-profile/council-viz/Network.js
@@ -2,32 +2,32 @@ import React from "react";
 import { Text } from "react-konva";
 
 class Network extends React.Component {
-  render() {
-    let angle = 180 * 0.0174533 - (1 / 5) * Math.PI;
-    const maxAngle = (1 / 5) * 2 * Math.PI;
-    const arr = [];
-    const reversedName = this.props.NetworkName.split("").reverse();
+	render() {
+		let angle = 180 * 0.0174533 - (1 / 5) * Math.PI;
+		const maxAngle = (1 / 5) * 2 * Math.PI;
+		const arr = [];
+		const reversedName = this.props.networkInfo.name.split("").reverse();
 
-    const radius = 220;
+		const radius = 220;
 
-    reversedName.forEach((element, index) => {
-      angle += maxAngle / (Number(this.props.NetworkName.length) + 1);
-      arr.push(
-        <Text
-          x={radius * Math.sin(angle) + this.props.x}
-          y={radius * Math.cos(angle) + this.props.y}
-          text={element}
-          fill="#798594"
-          history={this.props.history}
-          angle={angle}
-          radius={200}
-          fontSize={6}
-        />
-      );
-    });
+		reversedName.forEach((element, index) => {
+			angle += maxAngle / (Number(reversedName.length) + 1);
+			arr.push(
+				<Text
+					x={radius * Math.sin(angle) + this.props.x}
+					y={radius * Math.cos(angle) + this.props.y}
+					text={element}
+					fill="#798594"
+					history={this.props.history}
+					angle={angle}
+					radius={200}
+					fontSize={6}
+				/>
+			);
+		});
 
-    return arr;
-  }
+		return arr;
+	}
 }
 
 export default Network;
