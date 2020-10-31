04e06e1034e9383dd1442b0dc123330e31c98f5d
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2020-10-31 16:56:46 +0530

    feat: dashboard footer + settings page added

---
+++ pages/settings.js
@@ -0,0 +1,20 @@
+import dynamic from "next/dynamic";
+import withDashboardLayout from "@components/common/layouts/dashboard";
+
+const Page = dynamic(
+	() => import("@components/common/page").then((mod) => mod.default),
+	{ ssr: false }
+);
+
+const SettingsComponent = dynamic(
+	() => import("@components/settings").then((mod) => mod.default),
+	{ ssr: false }
+);
+
+const Settings = () => (
+	<Page title="Home" layoutProvider={withDashboardLayout}>
+		{() => <SettingsComponent />}
+	</Page>
+);
+
+export default Settings;
