58d4950382691129574bf402dad04b2139e7b8e1
Sahil <sahilnanda1995@gmail.com>
2020-11-06 13:25:29 +0530

    feat: 🎸 design changes

--- components/overview/OverviewCards.js
+++ components/overview/OverviewCards.js
@@ -48,20 +48,20 @@ const OverviewCards = ({
 		}
 	}, [stashAccount, apiInstance]);
 	React.useEffect(() => {
-		if (stats.totalAmountStaked) {
+		if (stats) {
 			convertCurrency(
 				stats.totalAmountStaked,
 				networkInfo.denom
 			).then((value) => setTotalAmountStakedFiat(value));
 		}
 
-		if (stats.estimatedRewards) {
+		if (stats) {
 			convertCurrency(stats.estimatedRewards, networkInfo.denom).then((value) =>
 				setEstimatedRewardsFiat(value)
 			);
 		}
 
-		if (validators && stats.totalAmountStaked) {
+		if (validators) {
 			calculateReward(
 				validators.filter((validator) => validator.isElected),
 				stats.totalAmountStaked,
@@ -72,7 +72,7 @@ const OverviewCards = ({
 			).then(({ yieldPercentage }) => setExpectedAPR(yieldPercentage));
 		}
 
-		if (stats.earnings) {
+		if (stats) {
 			convertCurrency(stats.earnings, networkInfo.denom).then((value) =>
 				setEarningsFiat(value)
 			);
@@ -80,29 +80,14 @@ const OverviewCards = ({
 	}, [stats, compounding]);
 
 	return (
-		<div className="flex justify-between items-center">
+		<div className="flex justify-between items-center h-full">
 			<div
-				className="bg-white min-h-12-rem py-4 px-8 text-center flex flex-col justify-center shadow-custom rounded-xl"
+				className="bg-white min-h-12-rem py-4 px-8 text-center flex flex-col justify-center shadow-custom rounded-xl h-full"
 				// style={{ width: "30%" }}
 			>
 				<div className="flex flex-col items-center justify-between">
 					<p className="semi-heading mt-40">Your investment</p>
 					<div>
-						{/* {get(activeStake, "currency", 0) !==
-							get(bondedAmount, "currency", 0) && (
-							<span className="text-sm bg-gray-200 text-gray-700 px-4 py-1 rounded-lg">
-								Active stake:{" "}
-								{formatCurrency.methods.formatAmount(
-									Math.trunc(
-										Number(
-											get(activeStake, "currency", 0) *
-												10 ** networkInfo.decimalPlaces
-										)
-									),
-									networkInfo
-								)}
-							</span>
-						)} */}
 						<h1 className="big-heading">
 							{formatCurrency.methods.formatAmount(
 								Math.trunc(
@@ -142,92 +127,7 @@ const OverviewCards = ({
 						</button>
 					</div>
 				</div>
-				{/* <div className="mt-8 flex justify-between items-center">
-					<h5 className="text-gray-800">Bonded</h5>
-					<div className="flex items-center">
-						<Popover trigger="hover">
-							<PopoverTrigger>
-								<span className="cursor-help hover:underline text-gray-500 text-sm">
-									Unlocking: {totalUnlockingBalance}
-								</span>
-							</PopoverTrigger>
-							<PopoverContent
-								zIndex={4}
-								px="1.5rem"
-								py="0.5rem"
-								width="16rem"
-								rounded="1rem"
-							>
-								<div>
-									<h5 className="font-semibold text-gray-600">
-										Unlocking Balances:
-									</h5>
-									<div>
-										{unlockingBalances.map((balanceInfo, index) => (
-											<div
-												key={index}
-												className="flex items-center justify-around text-gray-600 text-sm"
-											>
-												<Clock className="text-gray-600 mr-2" size="0.75rem" />
-												<span>
-													{formatCurrency.methods.formatAmount(
-														balanceInfo.value,
-														networkInfo
-													)}
-												</span>
-												<span className="ml-2">ERA: {balanceInfo.era}</span>
-											</div>
-										))}
-									</div>
-								</div>
-							</PopoverContent>
-						</Popover>
-					</div>
-				</div> */}
 			</div>
-			{/* <div
-				className="bg-gray-100 min-h-12-rem py-4 px-8 flex flex-col justify-center rounded-xl"
-				style={{ width: "30%" }}
-			>
-				<h1 className="text-3xl text-teal-500 font-semibold">
-					{expectedAPR}%
-				</h1>
-				<h3 className="text-gray-500">
-					for active nominations
-				</h3>
-				<div className="mt-8 flex justify-between items-center">
-					<h5 className="text-gray-800">Estimated APR</h5>
-				</div>
-			</div> */}
-			{/* <div
-				className="shadow-teal bg-teal-500 text-white rounded-xl min-h-12-rem py-4 px-8 flex flex-col justify-center"
-				style={{ width: "30%" }}
-			>
-				<h1 className="text-3xl font-semibold">
-					{formatCurrency.methods.formatAmount(
-						Math.trunc(
-							Number(stats.earnings || 0) * 10 ** networkInfo.decimalPlaces
-						),
-						networkInfo
-					)}
-				</h1>
-				{earningsFiat && (
-					<h3 className="text-lg text-white opacity-75">
-						$
-						{formatCurrency.methods.formatNumber(earningsFiat.toFixed(2)) ||
-							0.0}
-					</h3>
-				)}
-				<div className="mt-8 flex justify-between items-center">
-					<h5>Earnings</h5>
-					<button
-						className="bg-white text-teal-500 rounded-full px-4 py-2"
-						onClick={openRewardDestinationModal}
-					>
-						Payment Destination
-					</button>
-				</div>
-			</div> */}
 		</div>
 	);
 };
--- components/overview/index.js
+++ components/overview/index.js
@@ -13,7 +13,7 @@ import {
 	useValidatorData,
 } from "@lib/store";
 import { useWalletConnect } from "@components/wallet-connect";
-import { get, noop } from "lodash";
+import { get, isNil, noop } from "lodash";
 import { decodeAddress, encodeAddress } from "@polkadot/util-crypto";
 import RewardDestinationModal from "./RewardDestinationModal";
 import EditControllerModal from "./EditControllerModal";
@@ -90,6 +90,7 @@ const Overview = () => {
 	useEffect(() => {
 		setLoading(true);
 		setError(false);
+		setAllNominations(null);
 		if (get(stashAccount, "address") && apiInstance) {
 			const kusamaAddress = encodeAddress(
 				decodeAddress(stashAccount.address),
@@ -252,65 +253,49 @@ const Overview = () => {
 				</span>
 			</div>
 		</div>
-	) : error ? (
-		<div className="flex-center w-full h-full">
-			<div className="flex-center flex-col">
-				<AlertTriangle size="2rem" className="text-orange-500" />
-				<span className="font-semibold text-red-600 text-lg mb-10">
-					Sorry, no data for your account since you don't have active
-					nominations! :(
-				</span>
-				<span
-					onClick={() => router.replace(Routes.CALCULATOR)}
-					className="text-sm text-gray-600 mt-5 hover:underline cursor-pointer"
-				>
-					Use Reward Calculator to bond more funds and nominate.
-				</span>
-			</div>
-		</div>
 	) : (
-		userData && (
-			<div className="px-10 py-10 w-full h-full">
-				<RewardDestinationModal
-					isOpen={isRewardDestinationModalOpen}
-					close={closeRewardDestinationModal}
-					onEditController={onEditController}
-				/>
-				<EditControllerModal
-					isOpen={editControllerModalOpen}
-					close={closeEditControllerModal}
-					networkInfo={networkInfo}
-				/>
-				<FundsUpdate
-					isOpen={fundsUpdateModalOpen}
-					close={closeFundsUpdateModal}
-					type={fundsUpdateModalType}
-					nominations={allNominationsData}
-					bondedAmount={bondedAmount}
-					networkInfo={networkInfo}
-				/>
-				<EditValidators
-					isOpen={editValidatorModalOpen}
-					close={closeEditValidatorsModal}
-					validators={validators}
-					validatorsLoading={validatorsLoading}
-					currentValidators={allNominationsData}
-					onChill={() => {
-						closeEditValidatorsModal();
-						setTimeout(() => toggleChillAlert(), 500);
-					}}
-					networkInfo={networkInfo}
-				/>
-				<ChillAlert isOpen={chillAlertOpen} close={closeChillAlert} />
+		<div className="px-10 py-10 w-full h-full">
+			<RewardDestinationModal
+				isOpen={isRewardDestinationModalOpen}
+				close={closeRewardDestinationModal}
+				onEditController={onEditController}
+			/>
+			<EditControllerModal
+				isOpen={editControllerModalOpen}
+				close={closeEditControllerModal}
+				networkInfo={networkInfo}
+			/>
+			<FundsUpdate
+				isOpen={fundsUpdateModalOpen}
+				close={closeFundsUpdateModal}
+				type={fundsUpdateModalType}
+				nominations={allNominationsData}
+				bondedAmount={bondedAmount}
+				networkInfo={networkInfo}
+			/>
+			<EditValidators
+				isOpen={editValidatorModalOpen}
+				close={closeEditValidatorsModal}
+				validators={validators}
+				validatorsLoading={validatorsLoading}
+				currentValidators={allNominationsData}
+				onChill={() => {
+					closeEditValidatorsModal();
+					setTimeout(() => toggleChillAlert(), 500);
+				}}
+				networkInfo={networkInfo}
+			/>
+			<ChillAlert isOpen={chillAlertOpen} close={closeChillAlert} />
+			<div className="flex-col">
 				<div className="flex">
-					<div className="flex flex-col w-1/2 mr-8 h-full">
+					<div className="flex w-1/2 mr-8">
 						<div className="w-full">
 							<OverviewCards
-								stats={userData.stats}
+								stats={isNil(userData) ? null : userData.stats}
 								bondedAmount={bondedAmount}
 								address={stashAccount.address}
 								activeStake={activeStake}
-								validators={userData.validatorsInfo}
+								validators={isNil(userData) ? null : userData.validatorsInfo}
 								unlockingBalances={unlockingBalances}
 								bondFunds={() => openFundsUpdateModal("bond")}
 								unbondFunds={() => openFundsUpdateModal("unbond")}
@@ -318,23 +303,44 @@ const Overview = () => {
 								networkInfo={networkInfo}
 							/>
 						</div>
-						<div className="w-full">
-							<div className="flex flex-col h-full mb-2">
-								<button
-									onClick={handleValToggle}
-									className="flex text-gray-600 text-xs mt-4"
-								>
-									<ChevronRight
-										size={16}
-										className={`transition ease-in-out duration-500 mr-2 ${
-											showValidators && "transform rotate-90"
-										}`}
-									/>
-									{showValidators ? "Hide" : "See your"} validators
-								</button>
-								<Collapse isOpen={showValidators}>
-									<div className="flex justify-between items-center">
-										{/* <div className="flex items-center">
+					</div>
+					<div className="flex ml-8 w-1/2">
+						{activeStake && validators && (
+							<EarningsOutput
+								networkDenom={networkInfo.denom}
+								networkUrl={networkInfo.coinGeckoDenom}
+								networkInfo={networkInfo}
+								validators={
+									isNil(userData)
+										? []
+										: userData.validatorsInfo.filter(
+												(validator) => validator.isElected
+										  )
+								}
+								inputValue={activeStake}
+								apiInstance={apiInstance}
+								address={stashAccount.address}
+							/>
+						)}
+					</div>
+				</div>
+				<div className="w-full">
+					<div className="flex flex-col h-full mb-2">
+						<button
+							onClick={handleValToggle}
+							className="flex text-gray-600 text-xs mt-4"
+						>
+							<ChevronRight
+								size={16}
+								className={`transition ease-in-out duration-500 mr-2 ${
+									showValidators && "transform rotate-90"
+								}`}
+							/>
+							{showValidators ? "Hide" : "See your"} validators
+						</button>
+						<Collapse isOpen={showValidators}>
+							<div className="flex justify-between items-center">
+								{/* <div className="flex items-center">
 									<h3 className="text-2xl">
 										My validators
 									</h3>
@@ -347,71 +353,54 @@ const Overview = () => {
 										</button>
 									)}
 								</div> */}
-										<div className="flex items-center">
-											<button
-												className={
-													selectedTab === Tabs.NOMINATIONS
-														? "text-gray-900 mx-2"
-														: "text-gray-500 mx-2"
-												}
-												onClick={() => setSelectedTab(Tabs.NOMINATIONS)}
-											>
-												Selected
-											</button>
-											<button
-												className={
-													selectedTab === Tabs.ACTIVE_VALIDATORS
-														? "text-gray-900 mx-2"
-														: "text-gray-500 mx-2"
-												}
-												onClick={() => setSelectedTab(Tabs.ACTIVE_VALIDATORS)}
-											>
-												Active
-											</button>
-										</div>
-									</div>
-									{selectedTab === Tabs.ACTIVE_VALIDATORS ? (
-										<NominationsTable
-											validators={userData.validatorsInfo}
-											networkInfo={networkInfo}
-										/>
-									) : (
-										allNominationsData && (
-											<AllNominations
-												nominations={allNominationsData}
-												networkInfo={networkInfo}
-											/>
-										)
-									)}
-								</Collapse>
+								<div className="flex items-center">
+									<button
+										className={
+											selectedTab === Tabs.NOMINATIONS
+												? "text-gray-900 mx-2"
+												: "text-gray-500 mx-2"
+										}
+										onClick={() => setSelectedTab(Tabs.NOMINATIONS)}
+									>
+										Selected
+									</button>
+									<button
+										className={
+											selectedTab === Tabs.ACTIVE_VALIDATORS
+												? "text-gray-900 mx-2"
+												: "text-gray-500 mx-2"
+										}
+										onClick={() => setSelectedTab(Tabs.ACTIVE_VALIDATORS)}
+									>
+										Active
+									</button>
+								</div>
 							</div>
-							{/* <div className="w-4/12">
+							{selectedTab === Tabs.ACTIVE_VALIDATORS ? (
+								<NominationsTable
+									validators={isNil(userData) ? [] : userData.validatorsInfo}
+									networkInfo={networkInfo}
+								/>
+							) : (
+								allNominationsData && (
+									<AllNominations
+										nominations={allNominationsData}
+										networkInfo={networkInfo}
+									/>
+								)
+							)}
+						</Collapse>
+					</div>
+					{/* <div className="w-4/12">
 						<ExpectedReturns
 							stats={userData.stats}
 							validators={userData.validatorsInfo}
 							networkInfo={networkInfo}
 						/>
 					</div> */}
-						</div>
-					</div>
-					<div className="flex ml-8 w-1/2">
-						{activeStake && validators && (
-							<EarningsOutput
-								networkDenom={networkInfo.denom}
-								networkUrl={networkInfo.coinGeckoDenom}
-								networkInfo={networkInfo}
-								validators={userData.validatorsInfo.filter(
-									(validator) => validator.isElected
-								)}
-								inputValue={activeStake}
-								apiInstance={apiInstance}
-								address={stashAccount.address}
-							/>
-						)}
-					</div>
 				</div>
 			</div>
-		)
+		</div>
 	);
 };
 
