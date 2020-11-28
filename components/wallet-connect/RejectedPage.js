1577bdc11599dc682f405119cb611ac756450350
Sahil <sahilnanda1995@gmail.com>
2020-11-28 23:31:19 +0530

    fix: 🐛 fixes issue 144
    ✅ Closes: #144

--- components/wallet-connect/RejectedPage.js
+++ components/wallet-connect/RejectedPage.js
@@ -1,4 +1,4 @@
-const RejectedPage = () => (
+const RejectedPage = ({ handleRecoveryAuth }) => (
 	<div className="pb-12 flex flex-col text-center items-center">
 		<img src="/images/polkadot-wallet-connect.png" width="200px" />
 		<h3 className="mt-4 text-xl font-semibold px-20 text-gray-700">
@@ -9,23 +9,18 @@ const RejectedPage = () => (
 			accounts. It allows you to securely sign transactions using these accounts
 			while maintaining complete control over your funds.
 		</span>
-		<span className="mt-4 px-4 text-xs text-gray-700 font-semibold">
-			Extension not installed?
-		</span>
 		<a
-			className="mt-2 px-16 py-3 bg-teal-500 text-white rounded-full"
+			className="mt-12 px-16 py-3 bg-teal-500 text-white rounded-full"
 			href="https://github.com/polkadot-js/extension#installation"
 			target="_blank"
 		>
 			Install extension
 		</a>
-		<span className="mt-4 px-4 text-xl text-gray-700 font-semibold">or</span>
-		<span className="mt-4 px-4 text-xs text-gray-700 font-semibold">
-			Accidently rejected the request for permission?
-		</span>
-		<span className="mt-2 px-4 text-sm text-gray-700">
-			Try restarting your browser or you can also toggle the extension on/off
-			from browser's extension settings.
+		<span
+			className="mt-4 px-4 text-xs text-gray-500 underline cursor-pointer"
+			onClick={handleRecoveryAuth}
+		>
+			Accidently rejected the permission request?
 		</span>
 		<span className="mt-4 text-sm text-gray-600">
 			Using a different wallet?{" "}
