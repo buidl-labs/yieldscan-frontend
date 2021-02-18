2451e14756f07ce76f091c28b272e5c1cdd23b1a
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2021-02-18 18:42:33 +0530

    removed device detect limitation

--- components/common/page.js
+++ components/common/page.js
@@ -9,27 +9,27 @@ window.setImmediate = (cb) => cb();
 const Page = ({ title, children, layoutProvider }) => {
 	const layoutedChild = layoutProvider ? layoutProvider(children) : children;
 
-	if (isMobile || isTablet) {
-		return (
-			<React.Fragment>
-				<Head>
-					<title>{title} - YieldScan</title>
-				</Head>
-				<div
-					className="flex-center flex-col bg-gray-100"
-					style={{ width: "100vw", height: "100vh" }}
-				>
-					<span className="text-lg text-teal-500 font-bold text-4xl mb-10">
-						YieldScan
-					</span>
-					<h1 className="text-xl text-center text-gray-800 px-5">
-						We don't support this device yet. To use YieldScan please visit us
-						on your desktop / laptop.
-					</h1>
-				</div>
-			</React.Fragment>
-		);
-	}
+	// if (isMobile || isTablet) {
+	// 	return (
+	// 		<React.Fragment>
+	// 			<Head>
+	// 				<title>{title} - YieldScan</title>
+	// 			</Head>
+	// 			<div
+	// 				className="flex-center flex-col bg-gray-100"
+	// 				style={{ width: "100vw", height: "100vh" }}
+	// 			>
+	// 				<span className="text-lg text-teal-500 font-bold text-4xl mb-10">
+	// 					YieldScan
+	// 				</span>
+	// 				<h1 className="text-xl text-center text-gray-800 px-5">
+	// 					We don't support this device yet. To use YieldScan please visit us
+	// 					on your desktop / laptop.
+	// 				</h1>
+	// 			</div>
+	// 		</React.Fragment>
+	// 	);
+	// }
 
 	useEffect(() => {
 		trackEvent(Events.PAGE_VIEW, { path: window.location.pathname });
